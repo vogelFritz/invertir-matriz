afb9d8b67f217a9dab9b211d59e8d93f4015eb02
Francisco Santin <franciscosantin2002@gmail.com>
2023-07-07 10:21:55 -0300

    prompt-dimension

--- src/app.js
+++ src/app.js
@@ -6,31 +6,34 @@ import { obtencionMatrizInversa } from './obtencion-matriz-inversa/matriz-invers
 import { construirHtmlInversa } from './presentation/matriz-inversa/mostrar-inv';
 // import { obtencionMatriz } from './obtencion-matriz-inversa/matriz-inversa';
 
-const divApp = document.querySelector('#app');
-let matInv, matriz = [], vec = [],
-    i, j; 
-const n = 3, m = 3;
+const divApp = document.querySelector('#app'),
+      botonInversa = construirBotInversa(),
+      divMatriz = document.createElement('div');
+let matInv, 
+    matriz = [], 
+    vec = [], 
+    n, j,
+    inputs;
 
-const divMatriz = document.createElement('div');
 divMatriz.className = 'matriz';
 divMatriz.id = 'input-mat';
 
-construirMatriz(divMatriz,3,3); // Consigo el div completo
+n = prompt('Ingrese las dimensiones de la matriz nxn', 3);
+n = parseFloat(n);
+
+construirMatriz(divMatriz,n,n); // Consigo el div completo
 
 divApp.append( divMatriz );
-
-const botonInversa = construirBotInversa();
 
 divApp.append( botonInversa );
 
 botonInversa.addEventListener( 'click', () => {
-    const inputs = document.querySelectorAll('.inputs-mat');
-    i = 0;
+    inputs = document.querySelectorAll('.inputs-mat');
     j = 0;
     inputs.forEach( elem => {
         vec.push(+elem.value);
         j++;
-        if(j >= m) {
+        if(j >= n) {
             matriz.push(vec);
             j = 0;
             vec = [];
